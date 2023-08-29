cbf1c13b0aaa5fcb1e5d6345b17615dc636e7917
Adnan Afzal <adnanafzal565@gmail.com>
2023-08-30 04:47:47 +0500

    Added text for customizations.

--- web/src/components/layouts/AppFooter.vue
+++ web/src/components/layouts/AppFooter.vue
@@ -1,9 +1,21 @@
 <template>
 	<footer class="container-fluid" style="margin-top: 50px; background-color: #e9e9e9; padding: 25px;">
-		<p class="text-center" style="margin-bottom: 0px;">
-			&copy;<span v-text="year"></span>&nbsp;
-			adnan-tech.com
-		</p>
+		<div class="row">
+			<div class="col-md-6">
+				<p style="margin-bottom: 0px; text-align: center;">
+					&copy;&nbsp;<span v-text="year"></span>&nbsp;
+					adnan-tech.com
+				</p>
+			</div>
+
+			<div class="col-md-6">
+				<p style="margin-bottom: 0px; text-align: left;">
+					For customizations, contact us: <br /><br />
+					<b>Email</b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; = &nbsp;&nbsp;&nbsp;&nbsp;support@adnan-tech.com <br />
+					<b>WhatsApp</b> &nbsp;&nbsp;&nbsp;&nbsp; = &nbsp;&nbsp;&nbsp;+92 310 5461304
+				</p>
+			</div>
+		</div>
 	</footer>
 </template>
 
